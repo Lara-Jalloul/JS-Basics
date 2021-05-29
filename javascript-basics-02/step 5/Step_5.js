c3bf6a2c0b492babb3e41d1a818a92ac767fa31c
Lara-Jalloul <laraadjalloul@gmail.com>
2021-05-30 02:15:02 +0300

    Completed step 5

---
+++ javascript-basics-02/step 5/Step_5.js
@@ -0,0 +1,12 @@
+let target = document.querySelector('img');
+target.addEventListener("mouseover", changeImage, false);
+
+
+function changeImage() {
+    document.getElementById("image1").src = "images/image1_2.jpg";
+    document.getElementById("image2").src = "images/image2_2.jpg";
+    document.getElementById("image3").src = "images/image3_2.jpg";
+    document.getElementById("image4").src = "images/image4_2.jpg";
+    document.getElementById("image5").src = "images/image5_2.jpg";
+}
+
