82a34938741919cbb0e232d408c96d6292630135
Lara-Jalloul <laraadjalloul@gmail.com>
2021-05-30 02:01:14 +0300

    Completed step 4

---
+++ javascript-basics-02/step 4/index.js
@@ -0,0 +1,11 @@
+function myFunction(){
+    if (confirm('Do you want to clear all input fields?')) {
+        document.getElementById("name").value = '';
+        document.getElementById("surname").value = '';
+        document.getElementById("city").value = '';
+    } 
+}
+
+
+
+ 
