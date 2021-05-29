a3a9fba26f57b8d2477992d5fdc72aa8959d91a9
Lara-Jalloul <laraadjalloul@gmail.com>
2021-05-29 23:05:13 +0300

    Step_5: Simple Math

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,6 @@
+function myFunction(){
+    var firstNb = document.getElementById("first_number").value;
+    var secondNb = document.getElementById("second_number").value;
+    var result = firstNb * secondNb;
+    alert(result);
+}
