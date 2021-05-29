40246b2e86b68960943cba9eeec4314cdf5e9706
Lara-Jalloul <laraadjalloul@gmail.com>
2021-05-29 23:52:26 +0300

    Step_7: Multiple operations

---
+++ javascript-basics-01/Step_7.js
@@ -0,0 +1,11 @@
+function myFunction(){
+    var shoeSize = document.getElementById("shoe_size").value;
+    var year = document.getElementById("year").value;
+    var multShoe = shoeSize * 2;
+    var add = multShoe + 5;
+    var mult = add * 50;
+    var sub = mult - year;
+    var ad = sub + 1766;
+    var result = ad;
+    alert(result);
+}
