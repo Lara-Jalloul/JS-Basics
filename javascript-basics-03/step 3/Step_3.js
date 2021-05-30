44efd91eea0663e1def371ed1391fa3b35cd8901
Lara-Jalloul <laraadjalloul@gmail.com>
2021-05-30 03:29:54 +0300

    Completed step 3

---
+++ javascript-basics-03/step 3/Step_3.js
@@ -0,0 +1,9 @@
+function ChangeColorGreen(){
+    document.getElementById("text").style.color="green";
+}
+function ChangeColorRed(){
+    document.getElementById("text").style.color="red";
+}
+function ChangeColorBlue(){
+    document.getElementById("text").style.color="blue";
+}
